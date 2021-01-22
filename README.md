d0d6abbe64be2deafc39f33322c1768fc5a286ab
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2021-01-22 14:43:12 +0100

    Initial commit of gh action for building docker image
    Signed-off-by: Christian Kreuzberger <christian.kreuzberger@dynatrace.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+# GH Action to Build Docker Images (with some Keptn internal stuff)
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/keptn/gh-action-build-docker-image)
+
+This repo provides a utility GitHub action for the [CI Workflow](https://github.com/keptn/keptn/tree/master/.github/workflows) in the [Keptn Project](https://github.com/keptn). Nevertheless, this action provides a very basic functionality, as in "Build a docker image and do some sanity checks".
+
+## Inputs
+
+* Todo
+
+## Outputs
+
+* Todo
+
+## Example usage
+
+Todo
---
+++ build_docker_image.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [[ "$#" -ne 4 ]]; then
+  echo "Usage: $0 IMAGE FOLDER VERSION DATETIME"
+  echo "      Example: $0 keptn/api api/ 1.2.3 20210101101210"
+  echo "      Your command: $0 $*"
+  exit 1
+fi
+
+# todo: not sure if we can use parameters like this
+# ${IMAGE}=$1 ${FOLDER}=$2 ${VERSION}=$3 ${DATETIME}=$4
+IMAGE=$1
+FOLDER=$2
+VERSION=$3
+DATETIME=$4
+
+# store pwd
+pwd=$(pwd)
+
+# ensure trailing slash in folder
+if [[ "${FOLDER}" != */ ]]; then
+  echo "Please ensure that FOLDER has a trailing slash, e.g., ./ or api/"
+  exit 1
+fi
+
+# ToDo: Where do we get the Manifest from?
+echo "Building Docker Image ${IMAGE}:${VERSION}.${DATETIME} from ${FOLDER}DOCKERFILE"
+
+# change into the subfolder where the needed files are hosted
+cd ./${FOLDER}
+
+# uncomment certain lines from Dockerfile that are for CI builds only
+sed -i '/#travis-uncomment/s/^#travis-uncomment //g' Dockerfile
+sed -i '/#build-uncomment/s/^#build-uncomment //g' Dockerfile
+cat MANIFEST
+docker build . -t "${IMAGE}:${VERSION}.${DATETIME}" -t "${IMAGE}:${VERSION}" --build-arg version="${VERSION}"
+
+if [[ $? -ne 0 ]]; then
+  echo "Failed to build Docker Image ${IMAGE}:${VERSION}.${DATETIME}, exiting"
+  echo "::error file=${FOLDER}/Dockerfile::Failed to build Docker Image"
+  exit 1
+fi
+
+# push all tags that we just built
+docker push "${IMAGE}:${VERSION}.${DATETIME}" && docker push "${IMAGE}:${VERSION}"
+
+if [[ $? -ne 0 ]]; then
+  echo "::warning file=${FOLDER}/Dockerfile::Failed to push ${IMAGE}:${VERSION}.${DATETIME} to DockerHub, continuing anyway"
+  echo "* Failed to push ${IMAGE}:${VERSION}.${DATETIME} and ${IMAGE}:${VERSION} (Source: ${FOLDER})" >> ${pwd}/docker_build_report.txt
+else
+  echo "* Pushed ${IMAGE}:${VERSION}.${DATETIME} and ${IMAGE}:${VERSION} (Source: ${FOLDER})" >> ${pwd}/docker_build_report.txt
+fi
+
+# change back to previous directory
+cd ${pwd}

71bf32135bb724c0589fe9930910ff5ddc40ec7e
Dominik Augustin <dom.augustin@gmx.at>
2021-05-25 17:52:38 +0200

    Add support for specifying Dockerfile name

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 if [[ "$#" -ne 4 ]]; then
-  echo "Usage: $0 IMAGE FOLDER VERSION DATETIME"
-  echo "      Example: $0 keptn/api api/ 1.2.3 20210101101210"
+  echo "Usage: $0 IMAGE FOLDER VERSION DATETIME FILE"
+  echo "      Example: $0 keptn/api api/ 1.2.3 20210101101210 Dockerfile"
   echo "      Your command: $0 $*"
   exit 1
 fi
 
 # todo: not sure if we can use parameters like this
-# ${IMAGE}=$1 ${FOLDER}=$2 ${VERSION}=$3 ${DATETIME}=$4
+# ${IMAGE}=$1 ${FOLDER}=$2 ${VERSION}=$3 ${DATETIME}=$4 ${FILE}=$5
 IMAGE=$1
 FOLDER=$2
 VERSION=$3
 DATETIME=$4
+FILE=$5
 
 # store pwd
 pwd=$(pwd)
@@ -24,21 +25,21 @@ if [[ "${FOLDER}" != */ ]]; then
 fi
 
 # ToDo: Where do we get the Manifest from?
-echo "Building Docker Image ${IMAGE}:${VERSION}.${DATETIME} from ${FOLDER}DOCKERFILE"
+echo "Building Docker Image ${IMAGE}:${VERSION}.${DATETIME} from ${FOLDER}${FILE}"
 
 # change into the subfolder where the needed files are hosted
 cd ./${FOLDER}
 
 # uncomment certain lines from Dockerfile that are for CI builds only
-sed -i '/#travis-uncomment/s/^#travis-uncomment //g' Dockerfile
-sed -i '/#build-uncomment/s/^#build-uncomment //g' Dockerfile
+sed -i '/#travis-uncomment/s/^#travis-uncomment //g' ${FILE}
+sed -i '/#build-uncomment/s/^#build-uncomment //g' ${FILE}
 cat MANIFEST
 
 docker build . -t "${IMAGE}:${VERSION}.${DATETIME}" -t "${IMAGE}:${VERSION}" --build-arg version="${VERSION}"
 
 if [[ $? -ne 0 ]]; then
   echo "Failed to build Docker Image ${IMAGE}:${VERSION}.${DATETIME}, exiting"
-  echo "::error file=${FOLDER}/Dockerfile::Failed to build Docker Image"
+  echo "::error file=${FOLDER}/${FILE}::Failed to build Docker Image"
   exit 1
 fi
 
@@ -46,7 +47,7 @@ fi
 docker push "${IMAGE}:${VERSION}.${DATETIME}" && docker push "${IMAGE}:${VERSION}"
 
 if [[ $? -ne 0 ]]; then
-  echo "::warning file=${FOLDER}/Dockerfile::Failed to push ${IMAGE}:${VERSION}.${DATETIME} to DockerHub, continuing anyway"
+  echo "::warning file=${FOLDER}/${FILE}::Failed to push ${IMAGE}:${VERSION}.${DATETIME} to DockerHub, continuing anyway"
   echo "* Failed to push ${IMAGE}:${VERSION}.${DATETIME} and ${IMAGE}:${VERSION} (Source: ${FOLDER})" >> ${pwd}/docker_build_report.txt
 else
   echo "* Pushed ${IMAGE}:${VERSION}.${DATETIME} and ${IMAGE}:${VERSION} (Source: ${FOLDER})" >> ${pwd}/docker_build_report.txt
